8e4da731338859fd59ad335b97d51604135cbfdb
chuliee <namcheoljung@naver.com>
2023-01-04 17:35:59 +0900

    [Auto] Update 230104

--- database_manager.py
+++ database_manager.py
@@ -27,17 +27,28 @@ class DatabaseClient:
                 options = ' ' + table_dict['options']
             query = 'SELECT {0} FROM {1}{2};'.format(columns, table, options)
             cur.execute(query)
-            names = list(map(lambda x: x[0], cur.description))
             data = cur.fetchall()
             return data, query
 
     def update(self, table_dict):
         with sqlite3.connect(self.database) as conn:
-            pass
+            cur = conn.cursor()
+            columns = ','.join(table_dict['columns'])
+            table = table_dict['table']
+            options = ''
+            if(table_dict['options']):
+                options = ' ' + table_dict['options']
+            query = 'SELECT {0} FROM {1}{2};'.format(columns, table, options)
 
     def delete(self, table_dict):
         with sqlite3.connect(self.database) as conn:
-            pass
+            cur = conn.cursor()
+            columns = ','.join(table_dict['columns'])
+            table = table_dict['table']
+            options = ''
+            if(table_dict['options']):
+                options = ' ' + table_dict['options']
+            query = 'SELECT {0} FROM {1}{2};'.format(columns, table, options)
     
     def column_name(self, table_dict):
         with sqlite3.connect(self.database) as conn:
@@ -54,7 +65,7 @@ class DatabaseClient:
 
 if __name__ == '__main__':
     DB = DatabaseClient('chinook.db')
-    # result = DB.read({'table': 'albums', 'columns': '*', 'options': 'WHERE AlbumId=14'})
-    result = DB.column_name({'table': 'albums', 'columns': '*', 'options': 'WHERE AlbumId=14'})
+    result = DB.read({'table': 'albums', 'columns': '*', 'options': 'WHERE AlbumId=14'})
+    # result = DB.column_name({'table': 'albums', 'columns': '*', 'options': 'WHERE AlbumId=14'})
     print(result[0])
     print(result[1])
